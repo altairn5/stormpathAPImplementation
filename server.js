f7885b12bd28271a2b1a2598a6db39652d745b44
Humberto Calero <altairn5@uga.edu>
2016-04-15 13:43:28 -0700

    basic implementation

--- server.js
+++ server.js
@@ -1,63 +1,79 @@
-/*
-* server.js
-*/
+  /*
+  * server.js
+  */
+/* Require Express and Other Modules */
+  var express = require('express'),
+  bodyParser = require('body-parser'),
+  cookieParser = require("cookie-parser"),
+  stormpath = require('express-stormpath'),
+  app = express();
 
-// require express and other modules
-var express = require('express'),
-bodyParser = require('body-parser'),
-cookieParser = require("cookie-parser"),
-stormpath = require('express-stormpath'),
-app = express();
+
+  /* Serve Static Files from Public Folder */
+  app.use('/static',express.static(__dirname + '/public'));
+  app.use('/scripts', express.static(__dirname + '/node_modules'));
+
+
+  /* Stormpath API Implementation */
+  app.use(stormpath.init(app,{
+    website: true,
+    web:{
+      login: {
+        enabled: true,
+        nextUri: "/show"
+      },
+      logout: {
+        enabled: true,
+        nextUri: '/login'
+      }
+    }
+  }));
+
+  /* Alternative Implementation With Enviroment Variables */
+
+            /*app.use(stormpath.init(app, {
+            apiKey: {
+            id: process.env.STORMPATH_CLIENT_APIKEY_ID,
+            secret: process.env.STORMPATH_CLIENT_APIKEY_SECRET
+            },
+            application: {
+            href: process.env.STORMPATH_APPLICATION_HREF
+            }
+          }));*/
+
+
+/* set view engine to ejs */
+  app.set('view engine', 'ejs');
+
+
+
+/* Basic Route */
+
+
+  app.all(['/','/show'], stormpath.loginRequired, stormpath.getUser, function (req, res){
   
+      var data = req.user.givenName;
+  
+      var user = data.slice(0,1).toUpperCase() + data.slice(1).toLowerCase();
+  
+      if(req.user){
 
-// serve static files from public folder
-app.use('/static',express.static(__dirname + '/public'));
-app.use('/scripts', express.static(__dirname + '/node_modules'));
-app.use(stormpath.init(app,{
-	website: true,
-	web:{
-		login: {
-			enabled: true,
-			nextUri: "/show"
-				},
-		logout: {
-		    enabled: true,
-		    nextUri: '/login'
-				 }
-		}
-	}));
+            res.render('show', {userInfo: user});
+      }
+      else{
 
-// app.use(stormpath.init(app, {
-//   apiKey: {
-//     id: process.env.STORMPATH_CLIENT_APIKEY_ID,
-//     secret: process.env.STORMPATH_CLIENT_APIKEY_SECRET
-//   },
-//   application: {
-//     href: process.env.STORMPATH_APPLICATION_HREF
-//   }
-// }));
+            res.direct('login');
+      }
 
-// set view engine to ejs
-app.set('view engine', 'ejs');
-
-
-
-app.all(['/','/show'], stormpath.loginRequired, stormpath.getUser, function (req, res){
-	var data = req.user.givenName;
-	// console.log(`this is the data = ${data}`);
-	var user = data.slice(0,1).toUpperCase() + data.slice(1).toLowerCase();
-	// console.log(`this is the user = ${user}`);
-	
- 	if(req.user){
-  		res.render('show', {userInfo: user});
- 	}
- 	else{
- 		res.direct('login');
- 	}
-
-})
-
-// listen on port 3000
-app.on('stormpath.ready', function() {
-  app.listen(3000);
 });
+
+  /*listen on port 3000*/
+
+  app.on('stormpath.ready', function() {
+
+      app.listen(3000);
+
+  });
+
+
+  
